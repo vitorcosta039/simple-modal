4c188b4278e2b37f938dcc682da0be9dbcae36ee
vitorcosta0399 <vitor@nerau.com.br>
2021-12-24 17:32:44 -0300

    chore: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+	entry: path.resolve(__dirname, './src/index.js'),
+	output: {
+		filename: 'simple-modal.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+		],
+	},
+};
